586b1626be0014654a343b52404eb91f1432886c
Sam Johnston <samj@samj.net>
2023-02-13 13:21:32 +0700

    Update summary.py
    Ask it to use openai

--- summary.py
+++ summary.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+import os
+import openai
 from io import StringIO
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-import requests
-import json
-import os
 
 # Set the API key for OpenAI
-API_KEY = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 def pdf_to_text(file_path):
     """Convert a PDF to text using pdfminer"""
@@ -28,28 +27,15 @@ def pdf_to_text(file_path):
 
 def summarize_text(text):
     """Summarize text using the OpenAI GPT-3 API"""
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {API_KEY}"
-    }
     model = "text-davinci-002"
-    prompt = {
-        "prompt": f"summarize: {text}",
-        "model": model,
-        "max_tokens": 100
-    }
-    response = requests.post("https://api.openai.com/v1/models/{model}/jobs", headers=headers, json=prompt)
-
-    if response.status_code == 200:
-        response_json = response.json()
-        return response_json["choices"][0]["text"]
-    else:
-        raise Exception(f"Error summarizing text: {response.text}")
+    prompt = f"summarize: {text}"
+    completions = openai.Completion.create(engine=model, prompt=prompt, max_tokens=100, n=1, stop=None, temperature=0.5)
+    message = completions.choices[0].text
+    return message
 
 if __name__ == "__main__":
     file_path = input("Enter the path to the PDF file: ")
     text = pdf_to_text(file_path)
-    print(text)
     summary = summarize_text(text)
     print("Summary:")
     print(summary)
