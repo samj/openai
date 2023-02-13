4fd04ab3f3666170070adabc0e38dfcd2825eb1c
Sam Johnston <samj@samj.net>
2023-02-13 13:16:36 +0700

    Update summary.py
    Use pdfminer instead of PyPDF2

--- summary.py
+++ summary.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-import os
-import PyPDF2
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 import requests
 import json
+import os
 
 # Set the API key for OpenAI
 API_KEY = os.environ.get('OPENAI_API_KEY')
 
 def pdf_to_text(file_path):
-    """Convert a PDF to text using PyPDF2"""
+    """Convert a PDF to text using pdfminer"""
+    text = ""
     with open(file_path, "rb") as f:
-        pdf = PyPDF2.PdfFileReader(f)
-        text = " ".join([pdf.getPage(page_num).extractText() for page_num in range(pdf.numPages)])
+        rsrcmgr = PDFResourceManager()
+        sio = StringIO()
+        device = TextConverter(rsrcmgr, sio, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(f):
+            interpreter.process_page(page)
+            text += sio.getvalue()
+        device.close()
+        sio.close()
     return text
 
 def summarize_text(text):
@@ -21,10 +33,12 @@ def summarize_text(text):
         "Authorization": f"Bearer {API_KEY}"
     }
     model = "text-davinci-002"
-    prompt = (f"summarize: {text}"
-              f"model: {model}"
-              f"max_tokens: 100")
-    response = requests.post("https://api.openai.com/v1/engines/{model}/jobs", headers=headers, data=prompt)
+    prompt = {
+        "prompt": f"summarize: {text}",
+        "model": model,
+        "max_tokens": 100
+    }
+    response = requests.post("https://api.openai.com/v1/models/{model}/jobs", headers=headers, json=prompt)
 
     if response.status_code == 200:
         response_json = response.json()
@@ -35,6 +49,7 @@ def summarize_text(text):
 if __name__ == "__main__":
     file_path = input("Enter the path to the PDF file: ")
     text = pdf_to_text(file_path)
+    print(text)
     summary = summarize_text(text)
     print("Summary:")
     print(summary)
